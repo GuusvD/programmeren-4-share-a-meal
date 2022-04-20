1adcdc46f42b36af23e480a9d1420224bd09b1f5
GuusvD <g.vandamme@student.avans.nl>
2022-04-20 17:33:27 +0200

    Changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const req = require('express/lib/request')
 const res = require('express/lib/response')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 let userArray = [
   {
