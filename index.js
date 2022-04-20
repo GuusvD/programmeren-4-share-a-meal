c3cc8d8585b133883009bd7d630939434671a391
GuusvD <g.vandamme@student.avans.nl>
2022-04-20 16:27:25 +0200

    First commit
    Basic Javascript functions

---
+++ index.js
@@ -0,0 +1,148 @@
+const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+
+let userArray = [
+  {
+    id: 0,
+    firstName: "John",
+    lastName: "Doe",
+    street: "Lovensdijkstraat 61",
+    city: "Breda",
+    isActive: true,
+    emailAdress: "j.doe@server/com",
+    password: "secret",
+    phoneNumber: "06 12345678"
+  },
+  {
+    id: 1,
+    firstName: "Rob",
+    lastName: "Doe",
+    street: "Hogeschoollaan 10",
+    city: "Breda",
+    isActive: false,
+    emailAdress: "r.doe@server/com",
+    password: "terces",
+    phoneNumber: "06 87654321"
+  }
+]
+
+const bodyparser = require("body-parser")
+app.use(bodyparser.json())
+
+let database = []
+let id = 0
+
+app.listen(port, () => {
+  console.log(`Share-a-meal app listening on port ${port}`)
+})
+
+// app.all('*', (req, res) => {
+//   console.log("Welcome!")
+
+//   res.status(200).json({
+//     status: 200,
+//     result: "Welcome!"
+//   })
+
+//   res.end()
+// })
+
+app.get('/api/user', (req, res) => {
+  let text;
+
+  for (let index = 0; index < userArray.length; index++) {
+    text += JSON.stringify(userArray[index]) + "\n"
+  }
+
+  res.send(text)
+
+  console.log("Got all the Users")
+
+  res.end()
+})
+
+app.get('/api/user/profile', (req, res) => {
+  console.log("Got the User profile")
+  res.send("Got the User profile")
+
+  res.end()
+})
+
+app.get('/api/user/:userId', (req, res) => {
+  if (req.params.userId > userArray.length - 1) {
+    console.log("Couldn't find the User")
+    res.send("Couldn't find the User")
+  } else {
+    res.send(userArray[req.params.userId])
+    console.log("Got the User by id")
+  }
+
+  res.end()
+})
+
+app.post("/api/movie", (req, res) => {
+  let movie = req.body
+  id++
+  movie = {
+    id,
+    ...movie,
+  }
+  database.push(movie)
+
+  console.log("Added a new Movie:")
+  console.log(database)
+
+  res.status(201).json({
+    status: 201,
+    result: movie
+  })
+
+  res.end()
+})
+
+app.get("/api/movie", (req, res) => {
+
+  res.status(200).json({
+    status: 200,
+    result: database
+  })
+
+  res.end()
+})
+
+app.get("/api/movie/:id", (req, res) => {
+  const id = req.params.id
+  let movie = database.filter((item) => item.id == movie.id)
+
+  if (movie.length > 0) {
+    console.log(movie)
+    res.status(200).json({
+      status: 200,
+      result: movie
+    })
+  } else {
+    res.status(404).json ({
+      status: 404,
+      result: `Movie with id ${id} not found`
+    })
+  }
+
+  res.end()
+})
+
+app.put("/api/user", (req, res) => {
+  console.log("Updated a User by id")
+  res.send("Updated a User by id")
+
+  res.end()
+})
+
+app.delete("/api/user", (req, res) => {
+  console.log("Deleted a User by id")
+  res.send("Deleted a User by id")
+
+  res.end()
+})
