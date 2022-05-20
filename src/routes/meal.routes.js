3a3ec21bfd6ea650deecc17449753dc010189022
GuusvD <g.vandamme@student.avans.nl>
2022-05-20 14:43:41 +0200

    Fixed get meal by id function

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const mealController = require('../controllers/meal.controller')
 
 //Get all meals
-router.get("/api/meal", mealController.getAllMeals)
+router.get('/api/meal', mealController.getAllMeals)
+
+//Get meal by id
+router.get('/api/meal/:id', mealController.getMealById)
 
 module.exports = router
