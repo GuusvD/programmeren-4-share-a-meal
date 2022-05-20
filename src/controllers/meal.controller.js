f94d679bc54b92e2c111b27e15f911710ff0fd29
GuusvD <g.vandamme@student.avans.nl>
2022-05-20 16:04:21 +0200

    Added insert meal function

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -1,6 +1,107 @@
+const assert = require('assert')
 const dbconnection = require('../../database/dbconnection')
 
+let id
+
 let controller = {
+    validateMealInsert: (req, res, next) => {
+        let meal = req.body
+        let { isVega, isVegan, isToTakeHome, dateTime, maxAmountOfParticipants, price, imageUrl, name, description, allergenes } = meal
+
+        try {
+            assert(typeof isVega === 'boolean', 'IsVega must be a boolean!')
+            assert(typeof isVegan === 'boolean', 'IsVegan must be a boolean!')
+            assert(typeof isToTakeHome === 'boolean', 'IsToTakeHome must be a boolean!')
+            assert(typeof dateTime === 'string', 'DateTime must be a string!')
+            assert(typeof maxAmountOfParticipants === 'integer', 'MaxAmountOfParticipants must be a integer!')
+            assert(typeof price === 'double', 'Price must be a double!')
+            assert(typeof imageUrl === 'string', 'ImageUrl must be a string!')
+            assert(typeof name === 'string', 'Name must be a string!')
+            assert(typeof description === 'string', 'Description must be a string!')
+            assert(typeof allergenes === 'string', 'Allergenes must be a string!')
+
+            next()
+        } catch (error) {
+            const errorFinal = {
+                status: 400,
+                message: error.message
+            }
+            next(errorFinal)
+        }
+    },
+    addMeal: (req, res) => {
+        let meal = req.body
+
+        dbconnection.getConnection(function (err, connection) {
+            if (err) throw err
+
+            connection.query('SELECT * FROM meal', function (error, results, fields) {
+                connection.release()
+
+                if (error) throw error
+
+                let highestId = -1
+
+                results.forEach(element => {
+                    if (element.id > highestId) {
+                        highestId = element.id
+                    }
+                })
+
+                id = highestId + 1
+
+                meal = {
+                    id,
+                    ...meal
+                }
+
+                dbconnection.getConnection(function (err, connection) {
+                    if (err) throw err
+
+                    connection.query(`SELECT * FROM user WHERE id = ${req.userId}`, function (error, results, fields) {
+                        connection.release()
+
+                        if (error) throw error
+
+                        let user = results[0]
+
+                        dbconnection.getConnection(function (err, connection) {
+                            if (err) throw err
+
+                            connection.query(`INSERT INTO meal (id, isVega, isVegan, isToTakeHome, dateTime, maxAmountOfParticipants, price, imageUrl, cookId, name, description, allergenes) VALUES ('${meal.id}', '${meal.isVega}', '${meal.isVegan}', '${meal.isToTakeHome}', '${meal.dateTime}', '${meal.maxAmountOfParticipants}', '${meal.price}', '${meal.imageUrl}, '${req.userId}', '${meal.name}', '${meal.description}', '${meal.allergenes}')`, function (error, results, fields) {
+                                connection.release()
+
+                                if (error) throw error
+
+                                dbconnection.getConnection(function (err, connection) {
+                                    if (err) throw err
+
+                                    connection.query(`SELECT * FROM user WHERE id = ${id}`, function (error, results, fields) {
+                                        connection.release()
+
+                                        if (error) throw error
+
+                                        let user = results[0]
+
+                                        if (user.isActive == 0) {
+                                            user.isActive = false
+                                        } else {
+                                            user.isActive = true
+                                        }
+
+                                        res.status(201).json({
+                                            status: 201,
+                                            result: user
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    },
     getAllMeals: (req, res) => {
         dbconnection.getConnection(function (err, connection) {
             if (err) throw err
--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const mealController = require('../controllers/meal.controller')
+const authController = require('../controllers/auth.controller')
 
 //Get all meals
 router.get('/api/meal', mealController.getAllMeals)
 
 //Get meal by id
 router.get('/api/meal/:id', mealController.getMealById)
+
+//Add a new meal
+router.post('/api/meal', authController.validateToken, mealController.validateMealInsert, mealController.addMeal)
 
 module.exports = router
