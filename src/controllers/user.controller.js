fd8ccce9eb45a6dff4206c75ccfc06e18af48406
GuusvD <g.vandamme@student.avans.nl>
2022-05-21 20:30:03 +0200

    Removed mailchecker package usage and replaced it with regex

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,5 @@
 const assert = require('assert')
 const dbconnection = require('../../database/dbconnection')
-const MailChecker = require('mailchecker')
 
 let id
 
@@ -16,7 +15,7 @@ let controller = {
             assert(typeof city === "string", "City must be a string!")
             assert(typeof password === "string", "Password must be a string!")
             assert(typeof emailAdress === "string", "Emailadress must be a string!")
-            assert(MailChecker.isValid(emailAdress), "Emailadress is not valid!")
+            assert(emailAdress.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/), "Emailadress is not valid!")
 
             next()
         } catch (error) {
@@ -33,7 +32,7 @@ let controller = {
 
         try {
             assert(typeof emailAdress === "string", "Emailadress must be a string!")
-            assert(MailChecker.isValid(emailAdress), "Emailadress is not valid!")
+            assert(emailAdress.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/), "Emailadress is not valid!")
 
             next()
         } catch (error) {
